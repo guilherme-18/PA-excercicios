0e8fc922b99941ff6d070731e805c0f42a16d6d9
guilherme-18 <guilhermessoares1818trabalhos@gmail.com>
2026-08-05 18:03:46 -0300

    Add files via upload

---
+++ prjLista04/src/prjLista04/exercicio1.java
@@ -0,0 +1,28 @@
+package prjLista04;
+
+import java.util.Scanner;
+
+public class exercicio1 {
+
+	public static void main(String[] args) {
+        Scanner idoso = new Scanner(System.in);
+        int anoA, anoN, idadeF;
+        
+        System.out.println("coloque o ano atual: ");
+        anoA = idoso.nextInt();
+        System.out.println("coloque o seu ano de nascimento: ");
+        anoN = idoso.nextInt();
+        
+        idadeF = anoA - anoN;
+        System.out.println("sua idade é: " + idadeF);
+        
+        if (idadeF < 18) {
+            System.out.println("você é menor de idade");
+        } else {
+            System.out.println("voce é maior de idade");
+        }
+        
+        idoso.close();
+    }
+	
+}
---
+++ prjLista04/src/prjLista04/exercicio3.java
@@ -0,0 +1,5 @@
+package prjLista04;
+
+public class exercicio3 {
+
+}
