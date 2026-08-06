d9ffab64bea4ba7ea03fcc1a9d3f2557a62c58cc
guilherme-18 <guilhermessoares1818trabalhos@gmail.com>
2026-08-06 18:49:25 -0300

    Add files via upload

---
+++ prjLista01/src/prjLista01/exercicio2.java
@@ -0,0 +1,15 @@
+package prjLista01;
+
+public class exercicio2 {
+
+	public static void main(String[] args) {
+		
+		int numero;
+		
+		numero = 20 + 20 + 20 + 7;
+		
+		System.out.println(numero);
+		
+	}
+
+}
---
+++ prjLista01/src/prjLista01/exercicio3.java
@@ -0,0 +1,16 @@
+package prjLista01;
+
+public class exercicio3 {
+
+	public static void main(String[] args) {
+		
+		int numero1, numero2;
+		
+		numero1 = 28;
+		numero2 = 29;
+		
+		System.out.println("a soma dos numeros é: " + (numero1 + numero2));
+		
+	}
+	
+}
